dbf67c33c408f32903b29b34f2d2a3772b495f08
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-20 23:41:33 +0100

    Hello NodeJS?

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var port = process.env.port || 1337;
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("Hello");
+    res.end();
+});
+
+server.listen(port);		
