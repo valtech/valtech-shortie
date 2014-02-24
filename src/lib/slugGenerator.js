00f0f13f3889ed6c62526a7137b62edb45766264
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-24 22:43:14 +0100

    implmented slugGenerator

---
+++ src/lib/slugGenerator.js
@@ -0,0 +1,12 @@
+﻿var slugLength = 5;
+var allowedChars = 'bcdfghjklmnpqrstvwxz';
+
+module.exports = function() {
+  var chars = [];
+  for (var i = 0; i < slugLength; i++) {
+    var charIndex = Math.floor(Math.random() * allowedChars.length);
+    var c = allowedChars[charIndex];
+    chars.push(c);
+  }
+  return chars.join('');
+};
