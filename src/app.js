a706db1a511acc7c88daf0bc4aed99efadbdcc82
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-24 21:50:24 +0100

    First unit test with Mocha and Supertest!

--- src/app.js
+++ src/app.js
@@ -31,3 +31,5 @@ app.delete('/:slug', redirectRoutes.delete);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+exports.app = app;
---
+++ src/tests/staticRoutes.js
@@ -0,0 +1,13 @@
+﻿var request = require('supertest'),
+    express = require('express');
+
+var app = require('../app').app;
+
+describe('GET /', function() {
+  it('should return html', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /text\/html/)
+      .expect(200, done);
+  });
+});
