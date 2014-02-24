3da161493d1fd2ce1c5ef611a4df568b76e1ee03
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-24 22:37:26 +0100

    added test for slugGenerator

---
+++ tests/lib/slugGenerator.js
@@ -0,0 +1,21 @@
+﻿var slugGenerator = require('../../src/lib/slugGenerator');
+var assert = require('chai').assert;
+var _ = require('underscore');
+
+describe('slugGenerator', function() {
+  describe('generated slug', function() {
+    it('should contain 5 consonants', function() {
+      var slug = slugGenerator();
+      assert.match(slug, /[bcdfghjklmnpqrstvwxz]{5}/);
+    });
+
+    it('should appear to be random', function() {
+      var n = 20;
+      var slugs = [];
+      while (n--) {
+        slugs.push(slugGenerator());
+      }
+      assert.equal(_.uniq(slugs).length, slugs.length);
+    });
+  });
+});
