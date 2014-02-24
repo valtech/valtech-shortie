20fa3630418766d433d157e4e14a60f364e7eed2
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-24 20:53:23 +0100

    Moved src files and added boilerplate express app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
 var http = require('http');
+var path = require('path');
 
-var port = process.env.PORT || 1337;
+var app = express();
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write("Hello");
-    res.end();
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade'); // later jade
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-server.listen(port);		
---
+++ src/routes/index.js
@@ -0,0 +1,3 @@
+﻿exports.index = function(req, res) {
+  res.sendfile('views/index.html');
+};
