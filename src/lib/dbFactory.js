9a0d8c3e4133dac66593898aff97a9c4bb223a80
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-25 10:54:05 +0100

    Added a factory for creating either an NeDb store or MongoDb store

---
+++ src/lib/dbFactory.js
@@ -0,0 +1,16 @@
+﻿var Datastore = require('nedb');
+//var MongoClient = require('mongodb').MongoClient;
+
+var dbFactory = function(type, options, callback) {
+  switch (type) {
+  case 'nedb':
+    var db = new Datastore(options);
+    callback(null, db);
+    break;
+  case 'mongodb':
+    // MongoClient.connect('somewhere', options, callback);
+    break;
+  }
+};
+
+module.exports = dbFactory;
---
+++ tests/db/neDbTests.js
@@ -0,0 +1,31 @@
+﻿var assert = require('chai').assert;
+var dbFactory = require('../../src/lib/dbFactory');
+
+describe('NeDb handler', function() {
+  var db;
+  beforeEach(function(done) {
+    dbFactory('nedb', {}, function(err, db_) {
+      db = db_;
+      done();
+    });
+  });
+  it('should be able to insert document and find it', function(done) {
+    db.insert({ foo: 'bar' }, function(err) {
+      assert.isNull(err, err);
+      db.findOne({ foo: 'bar' }, function(err, doc) {
+        assert.isNull(err, err);
+        assert.isNotNull(doc);
+        assert.equal(doc.foo, 'bar');
+        done();
+      });
+    });
+  });
+  it('should fail insert on unique violation', function(done) {
+    db.ensureIndex({ fieldName: 'foo', unique: true });
+    db.insert([{ foo: 'bar' }, { foo: 'bar' }], function(err) {
+      assert.isNotNull(err);
+      done();
+    });
+  });
+});
+
