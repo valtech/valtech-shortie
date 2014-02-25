4faab83f95c89a7ab6e02baf4c27f4852d96615a
Adam Renberg <tgwizard@gmail.com>
2014-02-25 21:01:13 +0100

    Add black valtech favicon
    Stolen from phoenix.valtech.se

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
