a0b4ab7b3c8fcec520c06b6f172279f1e78cfcb8
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-26 16:25:42 +0100

    Feature: Add redirect to DB

---
+++ src/lib/redirectRepository.js
@@ -0,0 +1,24 @@
+﻿var dbFactory = require('./dbFactory');
+
+var RedirectRepository = (function() {
+  function RedirectRepository(db) {
+    this.db = db;
+  }
+
+  RedirectRepository.prototype.addRedirect = function(redirect, callback) {
+    if(callback)
+      this.db.insert(redirect, callback);
+    else
+      this.db.insert(redirect);
+  };
+  RedirectRepository.prototype.getRedirectBySlug = function(slug) {
+    return this.db.findOne({ slug: slug });
+  };
+  RedirectRepository.prototype.getRedirectsByUrl = function(url) {
+    return this.db.find({ url: url });
+  };
+
+  return RedirectRepository;
+})();
+
+module.exports = RedirectRepository;
---
+++ tests/db/redirectRepository.js
@@ -0,0 +1,42 @@
+﻿var assert = require('chai').assert;
+var dbFactory = require('../../src/lib/dbFactory');
+var RedirectRepository = require('../../src/lib/redirectRepository');
+
+describe('RedirectRepository', function() {
+  var repo, db;
+  beforeEach(function(done) {
+    dbFactory('nedb', {}, function(err, db_) {
+      db = db_;
+      repo = new RedirectRepository(db_);
+      done();
+    });
+  });
+  
+  it('should be able to add a Redirect', function(done) {
+    var redirect = {      
+      url: 'http://icanhazcheezburger.com/',
+      slug: 'cats'
+    };
+    repo.addRedirect(redirect);
+    db.findOne({ slug: 'cats' }, function(err, doc) {
+      assert.isNull(err, err);
+      assert.isNotNull(doc);
+      assert.equal(doc.slug, 'cats');
+      done();
+    });
+  });
+  it('should fail if Redirect already exists', function(done) {
+    var redirect = {      
+      url: 'http://icanhazcheezburger.com/',
+      slug: 'cats'
+    };
+    repo.addRedirect(redirect);
+    repo.addRedirect(redirect, function(err, doc) {
+      assert.isNull(err, err);
+      assert.isNotNull(doc);
+      assert.equal(doc.slug, 'cats');
+      done();
+    });
+  });
+});
+
