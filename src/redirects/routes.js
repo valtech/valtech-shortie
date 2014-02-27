6bc2687f8cd0f725ec80bb1686ab601f39fb3fee
c.palmstierna <christian.palmstierna@valtech.se>
2014-02-27 20:55:02 +0100

    Converted redirects/routes to TypeScript

--- src/redirects/routes.js
+++ src/redirects/routes.js
@@ -1,24 +1,32 @@
-﻿exports.get = function(req, res, next) {
-  if (req.params.slug == 'cats') {
-    res.redirect('http://icanhazcheezburger.com/');
-    return;
-  }
-  next();
-};
+﻿function getHandler(req, res, next) {
+    if (req.params.slug == 'cats') {
+        res.redirect('http://icanhazcheezburger.com/');
+        return;
+    }
+    next();
+}
+exports.getHandler = getHandler;
 
-// all below methods require auth
-exports.post = function(req, res, next) {
-  // add new redirect with generated slug
-  // return 200 and redirect entity in body on success
-  // return 400 on invalid data
-  next();
-};
-exports.put = function(req, res, next) {
-  // update slug or add redirect with specified slug
-  // return 400 on slug/shortUrl mismatch
-  // return redirect entity in body on success
-};
-exports.delete = function(req, res, next) {
-  // delete redirect
-  // only returns 200 or 404
-};
+function postHandler(req, res, next) {
+    // require auth
+    // add new redirect with generated slug
+    // return 200 and redirect entity in body on success
+    // return 400 on invalid data
+    next();
+}
+exports.postHandler = postHandler;
+
+function putHandler(req, res, next) {
+    // require auth
+    // update slug or add redirect with specified slug
+    // return 400 on slug/shortUrl mismatch
+    // return redirect entity in body on success
+}
+exports.putHandler = putHandler;
+
+function deleteHandler(req, res, next) {
+    // require auth
+    // delete redirect
+    // only returns 200 or 404
+}
+exports.deleteHandler = deleteHandler;
---
+++ src/redirects/routes.ts
@@ -0,0 +1,28 @@
+﻿export function getHandler(req, res, next) {
+  if (req.params.slug == 'cats') {
+    res.redirect('http://icanhazcheezburger.com/');
+    return;
+  }
+  next();
+}
+
+export function postHandler(req, res, next) {
+  // require auth
+  // add new redirect with generated slug
+  // return 200 and redirect entity in body on success
+  // return 400 on invalid data
+  next();
+}
+
+export function putHandler(req, res, next) {
+  // require auth
+  // update slug or add redirect with specified slug
+  // return 400 on slug/shortUrl mismatch
+  // return redirect entity in body on success
+}
+
+export function deleteHandler(req, res, next) {
+  // require auth
+  // delete redirect
+  // only returns 200 or 404
+}
