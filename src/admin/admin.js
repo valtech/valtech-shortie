a155fe944427b293c89abf29633a66eb1e2d9964
par.dahlman <par.dahlman@valtech.se>
2014-03-01 20:16:23 +0100

    Removing generated javascript from source controll
    As they are generated.

--- src/admin/admin.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../.types/underscore.d.ts" />
-// This is for frontend logic
-var _ = require('underscore');
-var $ = require('jquery');
-
-_.each(['testing', 'browserify', 'through alerts'], function (x) {
-    window.alert(x);
-});
