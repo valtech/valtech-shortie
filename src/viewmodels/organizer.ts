4836c93f2d108e723176bc6a7f3b695e52bedbc2
par.dahlman <par.dahlman@valtech.se>
2014-03-01 20:21:10 +0100

    Adding a frontend 'organizer'
    Which is a viewmodel that holds all the shorites and will be the one handling
    organizatoin in frontend.

---
+++ src/viewmodels/organizer.ts
@@ -0,0 +1,23 @@
+﻿/// <reference path="../../.types/underscore.d.ts"/>
+/// <reference path="../../.types/node.d.ts"/>
+/// <reference path="../../.types/knockout.d.ts"/>
+/// <reference path="shortie-vm.ts"/>
+
+import knockout = require('knockout');
+import underscore = require('underscore');
+
+// this is a hack for better intellisence in vs2013
+var _: UnderscoreStatic = underscore;
+var ko: KnockoutStatic = knockout;
+
+import shortie = require('./shortie-vm');
+
+export class vm {
+	public shorties: KnockoutObservableArray<shortie.vm>;
+	public currentShortie: KnockoutObservable<shortie.vm>;
+
+	constructor(raws: Array<shortie.obj>) {
+		var arrayOfVms = _.map(raws, raw=> new shortie.vm(raw));
+		this.shorties = ko.observableArray(arrayOfVms);
+	}
+}
---
+++ test/viewmodels/organizer.ts
@@ -0,0 +1,38 @@
+﻿/// <reference path="../../.types/mocha.d.ts"/>
+/// <reference path="../../.types/sinon.d.ts"/>
+/// <reference path="../../.types/my-chai.d.ts"/>
+/// <reference path="../../src/viewmodels/shortie-vm.ts"/>
+/// <reference path="../../src/viewmodels/organizer.ts"/>
+
+import shortie = require('../../src/viewmodels/shortie-vm');
+import organizer = require('../../src/viewmodels/organizer');
+
+import chai = require('chai');
+var assert : Assert = chai.assert;
+
+import sinonModule = require('sinon');
+var sinon: SinonStatic = sinonModule;
+
+describe("The 'organizer'", ()=> {
+	var raws: Array<shortie.obj>;
+
+	beforeEach(()=> {
+		raws = [
+			new shortie.obj("lilla-anna", "http://sv.wikipedia.org/wiki/Lilla_Anna_och_Langa_farbrorn"),
+			new shortie.obj("go-shorty", "http://rapgenius.com/50-cent-in-da-club-lyrics"),
+			new shortie.obj("i-wish", "http://open.spotify.com/track/74WFSCXc8yHY7HDXREiLpM")
+		];
+	});
+
+	it("Should create vms for all raw shories in constructor", ()=> {
+		/* Setup */
+		var spy = sinon.spy(shortie.vm);
+		
+		/* Test */
+		var model = new organizer.vm(raws);
+
+		/* Assert */
+		//sinon.assert.callCount(spy, 3); // TODO: make this work.
+		assert.equal(model.shorties().length, 3);
+	});
+})
