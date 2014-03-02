a45fd943d4fe6131bf089b4b698af96659371bfb
c.palmstierna <christian.palmstierna@valtech.se>
2014-03-02 18:02:49 +0100

    Changed organizer.ts to use methods instead of lambda variables.
    Also, some unfortunate whitespace/tab changing.

--- src/viewmodels/organizer.ts
+++ src/viewmodels/organizer.ts
@@ -12,66 +12,65 @@ var ko: KnockoutStatic = knockout;
 
 import shortie = require('./shortie-vm');
 
-export class vm {
-	public shorties: KnockoutObservableArray<shortie.vm>;
-	public currentShortie: KnockoutObservable<shortie.vm>;
-	public spamWarning: KnockoutComputed<boolean>;
+export class ViewModel {
+  public shorties: KnockoutObservableArray<shortie.vm>;
+  public currentShortie: KnockoutObservable<shortie.vm>;
+  public spamWarning: KnockoutComputed<boolean>;
 
-	private spamAttemped: KnockoutObservable<boolean>;
-	private containsEmpties : KnockoutComputed<boolean>;
+  private spamAttemped: KnockoutObservable<boolean>;
+  private containsEmpties: KnockoutComputed<boolean>;
 
-	constructor(raws: Array<shortie.obj>) {
-		var arrayOfVms = _.map(raws, raw => new shortie.vm(raw));
-		this.shorties = ko.observableArray(arrayOfVms);
+  constructor(raws: Array<shortie.obj>) {
+    var arrayOfVms = _.map(raws, raw => new shortie.vm(raw));
+    this.shorties = ko.observableArray(arrayOfVms);
 
-		this.spamAttemped = ko.observable(false);
-		this.containsEmpties = ko.computed(()=> containsEmptyShorties(this.shorties()));
-		this.spamWarning = ko.computed(()=> this.spamAttemped() && this.containsEmpties());
-	}
+    this.spamAttemped = ko.observable(false);
+    this.containsEmpties = ko.computed(() => containsEmptyShorties(this.shorties()));
+    this.spamWarning = ko.computed(() => this.spamAttemped() && this.containsEmpties());
+  }
 
-	public select = (shortie: shortie.vm) => {
-		var current = _.find<shortie.vm>(this.shorties(), s=> s == shortie);
-		if (!current)
-			return;
+  public select(shortie: shortie.vm) : void {
+    var current = _.find<shortie.vm>(this.shorties(), s=> s == shortie);
+    if (!current)
+      return;
 
-		this.shorties().forEach(s=> s.isCurrent(false));
-		current.isCurrent(true);
-	};
+    this.shorties().forEach(s=> s.isCurrent(false));
+    current.isCurrent(true);
+  }
 
-	public addNew = () => {
-		if (this.containsEmpties()) {
-			this.spamAttemped(true);
-			selectFirstEmptyShorties(this.shorties());
-			return;
-		}
+  public addNew() : void {
+    if (this.containsEmpties()) {
+      this.spamAttemped(true);
+      selectFirstEmptyShorties(this.shorties());
+      return;
+    }
 
-		var newShortie = new shortie.vm();
-		this.shorties.push(newShortie);
-		this.select(newShortie);
-	}
+    var newShortie = new shortie.vm();
+    this.shorties.push(newShortie);
+    this.select(newShortie);
+  }
 
-	public save = (shortie: shortie.vm) => {
-		this.shorties().forEach(s=> s.isCurrent(false));
-	}
-
+  public save(shortie: shortie.vm) : void {
+    this.shorties().forEach(s=> s.isCurrent(false));
+  }
 }
 
-function containsEmptyShorties(shorties: Array <shortie.vm>) : boolean {
-	var hasEmpties = _.any<shortie.vm>(shorties,
-		shortie => { return !shortie.slug() || !shortie.fullUrl(); }
-	);
+function containsEmptyShorties(shorties: Array<shortie.vm>): boolean {
+  var hasEmpties = _.any<shortie.vm>(shorties,
+    shortie => { return !shortie.slug() || !shortie.fullUrl(); }
+    );
 
-	return hasEmpties;
+  return hasEmpties;
 }
 
 function selectFirstEmptyShorties(shorties: Array<shortie.vm>): void {
-	shorties.forEach(s=> s.isCurrent(false));
-	var firstEmpty = _.find<shortie.vm>(shorties, shortie=> {
-		if (!shortie.slug() || !shortie.fullUrl())
-			return true;
-		return false;
-	});
+  shorties.forEach(s=> s.isCurrent(false));
+  var firstEmpty = _.find<shortie.vm>(shorties, shortie=> {
+    if (!shortie.slug() || !shortie.fullUrl())
+      return true;
+    return false;
+  });
 
-	if (firstEmpty)
-		firstEmpty.isCurrent(true);
+  if (firstEmpty)
+    firstEmpty.isCurrent(true);
 }
