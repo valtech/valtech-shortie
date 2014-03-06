077a76ef8fb52c0d241797ad150eef4a9754a9ef
c.palmstierna <christian.palmstierna@valtech.se>
2014-03-06 12:23:19 +0100

    Testing logentries

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 ﻿var http = require('http');
 var app = require('./src/app');
+var logentries = require('node-logentries');
+
+var log = logentries.logger({
+    token: '9bb3dd10-4bfe-4fdb-b7ec-83c3b1fc26fc'
+});
 
 http.createServer(app.App).listen(app.App.get('port'), function () {
-    console.log('Express server listening on port ' + app.App.get('port'));
+    log.log('Express server listening on port ' + app.App.get('port'));
 });
--- server.ts
+++ server.ts
@@ -1,9 +1,14 @@
-﻿/// <reference path="../.types/node/node.d.ts" />
+﻿/// <reference path="./.types/node/node.d.ts" />
 
 import http = require('http');
 import app = require('./src/app');
+var logentries = require('node-logentries');
+
+var log = logentries.logger({
+  token:'9bb3dd10-4bfe-4fdb-b7ec-83c3b1fc26fc'
+});
 
 http.createServer(app.App).listen(app.App.get('port'), function () {
-  console.log('Express server listening on port ' + app.App.get('port'));
+  log.log('Express server listening on port ' + app.App.get('port'));
 });
 
