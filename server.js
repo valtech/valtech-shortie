139f86abeb6e6266ec3574a20d84dfb11c896765
c.palmstierna <christian.palmstierna@valtech.se>
2014-03-06 09:42:00 +0100

    Moved server.js to root

---
+++ server.js
@@ -0,0 +1,6 @@
+﻿var http = require('http');
+var app = require('./src/app');
+
+http.createServer(app.App).listen(app.App.get('port'), function () {
+    console.log('Express server listening on port ' + app.App.get('port'));
+});
