abbde96c8cdc2d410145e08cdc1c78913a091dc6
c.palmstierna <christian.palmstierna@valtech.se>
2014-03-06 09:35:48 +0100

    Moved server.js to root

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../.types/node/node.d.ts" />
 
 import http = require('http');
-import app = require('./app');
+import app = require('./src/app');
 
 http.createServer(app.App).listen(app.App.get('port'), function () {
   console.log('Express server listening on port ' + app.App.get('port'));
