0a5a9b718e6be52e4c3e9b1d8d3f1d1b641b74fb
Adam Renberg <tgwizard@gmail.com>
2014-03-08 19:06:30 +0100

    Make nedb ctor callback async
    To behave the same as the mongodb callback does.

--- src/lib/DbFactory.js
+++ src/lib/DbFactory.js
@@ -6,9 +6,12 @@ var MONGO_URL = 'mongodb://127.0.0.1:27017/valtech_shorties?w=1';
 function create(type, options, callback) {
     switch (type) {
         case 'nedb':
-            var inMemoryDb = new Datastore(options);
-            inMemoryDb.ensureIndex({ field: 'slug', unique: true });
-            return callback(null, inMemoryDb);
+            process.nextTick(function () {
+                var inMemoryDb = new Datastore(options);
+                inMemoryDb.ensureIndex({ field: 'slug', unique: true });
+                callback(null, inMemoryDb);
+            });
+            break;
         case 'mongodb':
             mongodb.MongoClient.connect(MONGO_URL, function (err, db) {
                 if (err)
--- src/lib/DbFactory.ts
+++ src/lib/DbFactory.ts
@@ -9,9 +9,13 @@ var MONGO_URL = 'mongodb://127.0.0.1:27017/valtech_shorties?w=1'
 export function create(type: string, options: any, callback: (err: any, db?: any) => void): void {
   switch (type) {
     case 'nedb':
-      var inMemoryDb = new Datastore(options);
-      inMemoryDb.ensureIndex({ field: 'slug', unique: true });
-      return callback(null, inMemoryDb);
+      process.nextTick(function() {
+        // Make the nedb callback async to behave the same as for mongodb
+        var inMemoryDb = new Datastore(options);
+        inMemoryDb.ensureIndex({ field: 'slug', unique: true });
+        callback(null, inMemoryDb);
+      });
+      break;
     case 'mongodb':
       mongodb.MongoClient.connect(MONGO_URL, (err, db) => {
         if (err) return callback(err);
