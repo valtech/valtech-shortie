43f347dde453a1f31635cf7c4cdb983d617e4a4d
Adam Renberg <tgwizard@gmail.com>
2014-06-05 17:31:56 +0200

    Update idp credentials

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -17,8 +17,8 @@ var IDP_AUTHORIZE_URL = IDP_BASE_URL + '/oauth2/authorize';
 var IDP_TOKEN_URL = IDP_BASE_URL + '/oauth2/token';
 var IDP_USERS_ME_URL = IDP_BASE_URL + '/api/users/me';
 
-var IDP_CLIENT_ID = process.env.IDP_CLIENT_ID || 'local.valtech.shortie';
-var IDP_CLIENT_SECRET = process.env.IDP_CLIENT_SECRET || 'wRNntVkUoiFuC8kJRNDOligNO6btLZrYQz7Oq1NX';
+var IDP_CLIENT_ID = process.env.IDP_CLIENT_ID || 'valtech.shortie.local';
+var IDP_CLIENT_SECRET = process.env.IDP_CLIENT_SECRET || 'OTQyMmEyY2UtMDM4OS00YjU5LThkNTMtOGY4YzcwYTg1NGVm';
 var IDP_CLIENT_REDIRECT_URI = process.env.IDP_CLIENT_REDIRECT_URI || 'http://localhost:3000/login/callback';
 
 function login(req, res, next) {
