92fab4fcc639c081ff7288ed6bd752bb7862e386
Jon Ekdahl <jon.ekdahl@gmail.com>
2014-08-25 22:50:09 +0200

    Set working dir  to '/vagrant' directory on ssh login.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Install packages
 apt-get update
 apt-get install -y nodejs
 apt-get install -y nodejs-legacy
 apt-get install -y npm
 apt-get install -y mongodb
+
+# set working dir to /vagrant on ssh login
+grep "cd /vagrant" .bashrc > /dev/null || echo "cd /vagrant" >> .bashrc
 
 npm install -g grunt grunt-cli
 
